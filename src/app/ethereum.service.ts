ac0ada3d9e86627cce383379479ad3f0df5e1f55
kyle <khughessean01@gmail.com>
2023-12-09 15:15:47 +0200

    fix typescript warnings

--- src/app/ethereum.service.ts
+++ src/app/ethereum.service.ts
@@ -2,17 +2,19 @@
 import { Injectable } from '@angular/core';
 import { ethers } from 'ethers';
 
+declare let window: any;
+
 @Injectable({
   providedIn: 'root',
 })
 export class EthereumService {
-  provider: ethers.providers.Web3Provider;
-  signer: ethers.Signer;
-  MoodContract: any; // Replace with the actual type from ethers.js
-  abi: Array;
+  private provider: ethers.BrowserProvider;
+  private signer!: ethers.Signer;
+  private MoodContract!: ethers.Contract;
+  private abi: Array<any>;
 
   constructor() {
-    this.provider = new ethers.providers.Web3Provider(window.ethereum, 'sepolia');
+    this.provider = new ethers.BrowserProvider(window.ethereum, 'sepolia');
     this.abi = [
       {
         "inputs": [
@@ -47,16 +49,20 @@ export class EthereumService {
   async init(): Promise<void> {
     await this.provider.send('eth_requestAccounts', []);
     const accounts = await this.provider.listAccounts();
-    this.signer = this.provider.getSigner(accounts[0]);
-    this.MoodContract = new ethers.Contract('0x5DB806eB4AD37747D37bcF5262f055FD65Ac938A', YOUR_CONTRACT_ABI, this.signer);
+    this.signer = await this.provider.getSigner();
+    this.MoodContract = new ethers.Contract('0x5DB806eB4AD37747D37bcF5262f055FD65Ac938A', this.abi, this.signer);
   }
 
   async getMood(): Promise<string> {
-    const mood = await this.MoodContract.getMood();
+    const mood = await this.MoodContract["getMood"]();
     return mood;
   }
 
   async setMood(newMood: string): Promise<void> {
-    await this.MoodContract.setMood(newMood);
+    await this.MoodContract['setMood'](newMood);
   }
 }
+
+/*
+Resource - https://stackoverflow.com/questions/75351780/nextjs-13-0-2-and-ethers-why-is-my-provider-undefined#:~:text=If%20you%20are,on%20ethers%20documentation
+*/
